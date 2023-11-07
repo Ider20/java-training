ca237c5ee668cc084e9334a24af4d3991cad7378
Ider <ider@Iders-MacBook-Pro.local>
2023-11-07 14:26:18 +0800

    new birthday theory from Temuujin

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 // const date = new Date("2014/03/15");
 // console.log(date);
-
 // const today = new Date();
 // const yyyy = today.getFullYear();
 // let mm = today.getMonth();
@@ -8,7 +7,6 @@
 // let hh = today.getHours();
 // let mi = today.getMinutes();
 // let se = today.getSeconds();
-
 // const formatted = dd + "-" + mm + "-" + yyyy + " " + hh + ":" + mi + ":" + se;
 // console.log(formatted);
 
@@ -30,19 +28,19 @@
 // console.log("нас -", age);
 
 // Birthday
-// const today = new Date();
-// const birthDate = new Date(1987, 11, 20);
-// if (birthDate.getFullYear() < today.getFullYear()) {
-//   let difference = today.getFullYear() - birthDate.getFullYear();
-//   birthDate.setFullYear(birthDate.getFullYear() + difference);
-//   if (birthDate.getMonth() < today.getMonth()) {
-//     birthDate.setFullYear(birthDate.getFullYear() + 1);
-//   }
-//   //   console.log(birthDate.getFullYear());
-// }
-// const oneDay = 1000 * 60 * 60 * 24;
-// const diff = Math.ceil((birthDate.getTime() - today.getTime()) / oneDay);
-// console.log("Төрсөн өдөр хүртэл", diff, "хоног үлдсэн байна");
+const today = new Date();
+const birthDate = new Date("1987/12/01");
+if (birthDate.getFullYear() < today.getFullYear()) {
+  let difference = today.getFullYear() - birthDate.getFullYear();
+  birthDate.setFullYear(birthDate.getFullYear() + difference);
+  if (birthDate.getMonth() < today.getMonth()) {
+    birthDate.setFullYear(birthDate.getFullYear() + 1);
+  }
+  //   console.log(birthDate.getFullYear());
+}
+const oneDay = 1000 * 60 * 60 * 24;
+const diff = Math.ceil((birthDate.getTime() - today.getTime()) / oneDay);
+console.log("Төрсөн өдөр хүртэл", diff, "хоног үлдсэн байна");
 
 // Exact day
 // const today = new Date();
@@ -114,36 +112,49 @@
 // let day = weekDays[birthDate.getUTCDay()];
 // console.log(day);
 
-// CountDown Timer
-const newdate = new Date("2023/11/20");
-console.log(
-  newdate.getFullYear() + "он",
-  newdate.getMonth() + "сар",
-  newdate.getDate(),
-  "өдөр хүртэл:"
-);
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-let timer;
-timer = setInterval(countDown, second);
-function countDown() {
-  const today = new Date();
-  let time = Math.abs(newdate - today);
-  //   console.log(time);
-  let days = Math.floor(time / day);
-  let hours = Math.floor((time % day) / hour);
-  let minutes = Math.floor((time % hour) / minute);
-  let seconds = Math.floor((time % minute) / second);
-  console.log(
-    days + "өдөр",
-    hours + "цаг",
-    minutes + "минут",
-    seconds + "секунд"
-  );
-  if (time <= 0) {
-    clearInterval(timer);
-    return "Энэ өдөр болов";
+// // CountDown Timer
+// const newdate = new Date("2023/11/20");
+// console.log(
+//   newdate.getFullYear() + "он",
+//   newdate.getMonth() + "сар",
+//   newdate.getDate(),
+//   "өдөр хүртэл:"
+// );
+// const second = 1000;
+// const minute = second * 60;
+// const hour = minute * 60;
+// const day = hour * 24;
+// let timer;
+// timer = setInterval(countDown, second);
+// function countDown() {
+//   const today = new Date();
+//   let time = Math.abs(newdate - today);
+//   //   console.log(time);
+//   let days = Math.floor(time / day);
+//   let hours = Math.floor((time % day) / hour);
+//   let minutes = Math.floor((time % hour) / minute);
+//   let seconds = Math.floor((time % minute) / second);
+//   console.log(
+//     days + "өдөр",
+//     hours + "цаг",
+//     minutes + "минут",
+//     seconds + "секунд"
+//   );
+//   if (time <= 0) {
+//     clearInterval(timer);
+//     return "Энэ өдөр болов";
+//   }
+// }
+
+// Birthday
+
+function calcDays(x, a, y, c) {
+  let calc = 0;
+  calc = calc = (x - 1) * 30 + a - ((y - 1) * 30 + c);
+  if (calc <= 0) {
+    let final = 365 + calc;
+    console.log(final);
   }
+  console.log(calc);
 }
+calcDays(12, 1, 11, 7);
